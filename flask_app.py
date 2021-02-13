ced1498018dd8f385dfd76f5534dd1b544bccb16
Kasper Biegun <kasper.biegun95@gmail.com>
2021-02-13 09:47:03 +0100

    Pages structure prepared

--- flask_app.py
+++ flask_app.py
@@ -8,5 +8,15 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/latest")
+def latest():
+    return render_template("latest.html")
+
+
+@app.route("/historical")
+def historical():
+    return render_template("historical.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
