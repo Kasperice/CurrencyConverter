452be821c07d357581012e6d569f55dae18b9519
Kasper Biegun <kasper.biegun95@gmail.com>
2021-02-13 19:25:23 +0100

    Displaying result corrected

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, render_template, request
 from currency_converter import CurrencyRates
 import datetime
 
@@ -23,8 +23,16 @@ def latest():
         quantity = request.form.get('quantity')
         curr1 = request.form.get('curr1').split()[0]
         curr2 = request.form.get('curr2').split()[0]
-        print(curr1, curr2)
-        result = c.get_latest_rate(curr1, curr2) * float(quantity)
+        result = ""
+        if 'submit_button' in request.form:
+            result = str(f"{quantity} {curr1} is equal to {float(quantity) * c.get_latest_rate(curr1, curr2):.2f} {curr2}")
+        elif 'submit_button2' in request.form:
+            result = str(f"{quantity} {curr2} is equal to {float(quantity) * c.get_latest_rate(curr2, curr1):.2f} {curr1}")
+        elif 'clear_button' in request.form:
+            result = ""
+            quantity = 1.0
+            curr1 = "EUR"
+            curr2 = "PLN"
         return render_template("latest.html", currencies=choices, test_value=result, defaults=(quantity, curr1, curr2))
     else:
         return render_template("latest.html", currencies=choices, defaults=(quantity, curr1, curr2))
@@ -36,14 +44,26 @@ def historical():
     curr1 = "EUR"
     curr2 = "PLN"
     date = datetime.date.today().strftime("%Y-%m-%d")
-    print(date)
+    time = 'is'
     if request.method == "POST":
         date = request.form.get('date')
         quantity = request.form.get('quantity')
         curr1 = request.form.get('curr1').split()[0]
         curr2 = request.form.get('curr2').split()[0]
-        print(curr1, curr2)
-        result = c.get_historical_rate(curr1, curr2, date) * float(quantity)
+        result = ""
+        if date != datetime.date.today().strftime("%Y-%m-%d"):
+            time = 'was'
+        if 'submit_button' in request.form:
+            result = str(f"{quantity} {curr1} {time} equal to {float(quantity) * c.get_historical_rate(curr1, curr2, date):.2f} {curr2}")
+        elif 'submit_button2' in request.form:
+            result = str(f"{quantity} {curr2} {time} equal to {float(quantity) * c.get_historical_rate(curr2, curr1, date):.2f} {curr1}")
+        elif 'clear_button' in request.form:
+            result = ""
+            quantity = 1.0
+            curr1 = "EUR"
+            curr2 = "PLN"
+            date = datetime.date.today().strftime("%Y-%m-%d")
+
         return render_template("historical.html", currencies=choices, test_value=result, defaults=(quantity, curr1, curr2, date))
     else:
         return render_template("historical.html", currencies=choices, defaults=(quantity, curr1, curr2, date))
