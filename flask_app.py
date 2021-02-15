3bd29647b9bc2db74dcf28038a628bc0ed318056
Kasper Biegun <kasper.biegun95@gmail.com>
2021-02-15 19:55:50 +0100

    Login with password implemented

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from currency_converter import CurrencyRates
+from flask_sqlalchemy import SQLAlchemy
 import datetime
 import uuid
+import hashlib
+
 
 app = Flask(__name__)
 app.secret_key = uuid.uuid4().hex
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = datetime.timedelta(days=7)
+
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
 
 c = CurrencyRates()
 choices = [f"{element} - {c.translate_currency_symbol(element)}" for element in list(c.get_latest_rates('EUR').keys())]
@@ -86,9 +106,25 @@ def login():
     if request.method == "POST":
         session.permanent = True
         user = request.form.get('username')
-        passw = request.form.get('pass')
+        passw = hashlib.md5(request.form.get('passw').encode()).hexdigest()
         session["user"] = user
-        flash("Login successfull!", "info")
+        found_user = Users.query.filter_by(name=user).first()
+        if 'login_button' in request.form:
+            if found_user and found_user.password == passw:
+                session["email"] = found_user.email
+                flash("Login successfull!", "info")
+            elif found_user and found_user.password != passw:
+                flash("Wrong password", "warning")
+                return render_template("login.html", login=user)
+            else:
+                flash("User does not exist, please register first", "warning")
+                return render_template("login.html", login=user)
+        elif 'register_button' in request.form:
+            usr = Users(user, None, passw)
+            db.session.add(usr)
+            db.session.commit()
+            flash("Registered successfully!", "info")
+
         return redirect(url_for("user"))
     else:
         if "user" in session:
@@ -97,11 +133,22 @@ def login():
         return render_template("login.html")
 
 
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        return render_template("user.html", user=user)
+        if request.method == "POST":
+            email = request.form.get("email")
+            session["email"] = email
+            found_user = Users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+            flash("Email was saved.", "info")
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html", user=user, email=email)
     else:
         return redirect(url_for("login"))
 
@@ -109,9 +156,17 @@ def user():
 @app.route("/logout")
 def logout():
     session.pop("user", None)
+    session.pop("email", None)
     flash("You have been logged out!", "success")
     return redirect(url_for("home"))
 
 
+@app.route("/view")
+def view():
+    return render_template("view.html", values=Users.query.all())
+
+
 if __name__ == "__main__":
+    db.create_all()
+    # db.drop_all()
     app.run(debug=True)
