39c710e18c617a0c798e23f6dbd511ad2ddde055
Kasper Biegun <kasper.biegun95@gmail.com>
2021-02-17 21:07:38 +0100

    Show conversion history in user view implemented

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import datetime
 import uuid
 import hashlib
-
+import re
 
 app = Flask(__name__)
 app.secret_key = uuid.uuid4().hex
@@ -29,6 +29,16 @@ class Users(db.Model):
         self.query_result = query_result
 
 
+def divide_entries(data, number_of_entries):
+    searches = list(data.split(" | "))[:number_of_entries]
+    results = dict()
+    for s in searches:
+        res = re.search("\[(.*?)\] (.*?)(?=on)on (.*?)$", s)
+        if res:
+            results[res.group(1)] = [res.group(2), res.group(3)]
+    return results
+
+
 c = CurrencyRates()
 choices = [f"{element} - {c.translate_currency_symbol(element)}" for element in list(c.get_latest_rates('EUR').keys())]
 choices.append("EUR - Euro")
@@ -36,7 +46,8 @@ choices.append("EUR - Euro")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html",
+                           logged="user" in session)
 
 
 @app.route("/latest", methods=["POST", "GET"])
@@ -64,13 +75,21 @@ def latest():
         if "user" in session and result != "":
             user = session["user"]
             found_user = Users.query.filter_by(name=user).first()
-            found_user.query_result += f"{result} | "
-            print(found_user.query_result)
+            date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            date = datetime.date.today().strftime("%Y-%m-%d")
+            found_user.query_result = f"[{date_time}] {result} on {date} | " + found_user.query_result
             db.session.commit()
 
-        return render_template("latest.html", currencies=choices, test_value=result, defaults=(quantity, curr1, curr2))
+        return render_template("latest.html",
+                               currencies=choices,
+                               test_value=result,
+                               defaults=(quantity, curr1, curr2),
+                               logged="user" in session)
     else:
-        return render_template("latest.html", currencies=choices, defaults=(quantity, curr1, curr2))
+        return render_template("latest.html",
+                               currencies=choices,
+                               defaults=(quantity, curr1, curr2),
+                               logged="user" in session)
 
 
 @app.route("/historical", methods=["POST", "GET"])
@@ -81,19 +100,19 @@ def historical():
     date = datetime.date.today().strftime("%Y-%m-%d")
     time = 'is'
     if request.method == "POST":
-        date = request.form.get('date')
+        historical_date = request.form.get('date')
         quantity = request.form.get('quantity').replace(',', '.')
         curr1 = request.form.get('curr1').split()[0]
         curr2 = request.form.get('curr2').split()[0]
         result = ""
-        if date != datetime.date.today().strftime("%Y-%m-%d"):
+        if historical_date != datetime.date.today().strftime("%Y-%m-%d"):
             time = 'was'
         if 'submit_button' in request.form:
             result = str(f"{quantity} {curr1} {time} equal to "
-                         f"{float(quantity) * c.get_historical_rate(curr1, curr2, date):.2f} {curr2}")
+                         f"{float(quantity) * c.get_historical_rate(curr1, curr2, historical_date):.2f} {curr2}")
         elif 'submit_button2' in request.form:
             result = str(f"{quantity} {curr2} {time} equal to "
-                         f"{float(quantity) * c.get_historical_rate(curr2, curr1, date):.2f} {curr1}")
+                         f"{float(quantity) * c.get_historical_rate(curr2, curr1, historical_date):.2f} {curr1}")
         elif 'clear_button' in request.form:
             result = ""
             quantity = 1.0
@@ -101,14 +120,23 @@ def historical():
             curr2 = "PLN"
             date = datetime.date.today().strftime("%Y-%m-%d")
 
+        if "user" in session and result != "":
+            user = session["user"]
+            found_user = Users.query.filter_by(name=user).first()
+            date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            found_user.query_result = f"[{date_time}] {result} on {historical_date} | " + found_user.query_result
+            db.session.commit()
+
         return render_template("historical.html",
                                currencies=choices,
                                test_value=result,
-                               defaults=(quantity, curr1, curr2, date))
+                               defaults=(quantity, curr1, curr2, date),
+                               logged="user" in session)
     else:
         return render_template("historical.html",
                                currencies=choices,
-                               defaults=(quantity, curr1, curr2, date))
+                               defaults=(quantity, curr1, curr2, date),
+                               logged="user" in session)
 
 
 @app.route("/login", methods=["POST", "GET"])
@@ -126,10 +154,12 @@ def login():
                 flash("Login successful!", "info")
             elif found_user and found_user.password != passw:
                 flash("Wrong password", "warning")
-                return render_template("login.html", login=user)
+                return render_template("login.html",
+                                       login=user)
             else:
                 flash("User does not exist, please register first", "warning")
-                return render_template("login.html", login=user)
+                return render_template("login.html",
+                                       login=user)
         elif 'register_button' in request.form:
             usr = Users(user, None, passw, "")
             db.session.add(usr)
@@ -159,7 +189,15 @@ def user():
         else:
             if "email" in session:
                 email = session["email"]
-        return render_template("user.html", user=user, email=email)
+
+        found_user = Users.query.filter_by(name=user).first()
+        results = divide_entries(found_user.query_result, 15)
+
+        return render_template("user.html",
+                               user=user,
+                               email=email,
+                               searches=results,
+                               logged="user" in session)
     else:
         return redirect(url_for("login"))
 
@@ -174,7 +212,9 @@ def logout():
 
 @app.route("/view")
 def view():
-    return render_template("view.html", values=Users.query.all())
+    return render_template("view.html",
+                           values=Users.query.all(),
+                           logged="user" in session)
 
 
 if __name__ == "__main__":
