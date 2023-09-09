94ac5f5cc2161b48c4495902c3093e6bd3603ecc
Kasper Biegun <kasper.biegun95@gmail.com>
2023-09-09 10:48:34 +0200

    Refactor

--- currency_converter/currency_converter.py
+++ currency_converter/currency_converter.py
@@ -1,43 +1,58 @@
-import requests
-from dotenv import load_dotenv
+"""currency_converter module."""
 import os
 
+import requests
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class CurrencyRatesAPIConnection:
+    """Connection to CurrencyRatesAPI."""
+
     source_address = "https://api.apilayer.com/exchangerates_data/"
 
     def __init__(self):
+        """Currency Rates API Connection initialization."""
         self.headers = {"apikey": os.environ.get("API_KEY")}
 
     def get_currency_names_and_symbols(self):
-        response = requests.request(
-            "GET", url=f"{self.source_address}symbols", headers=self.headers
+        """Get available currency names and their symbols."""
+        response = requests.get(
+            url=f"{self.source_address}symbols", headers=self.headers
         )
-        if response.status_code == 200:
+        if response.status_code == requests.codes.ok:
             return response.json()["symbols"]
         else:
             raise ValueError(
-                f"Wrong status code. Expected: 200, Actual {response.status_code}"
+                f"Wrong status code. Expected: {requests.codes.ok}, Actual {response.status_code}"
             )
 
-    def get_exchange_rate(self, first_currency, second_currency, date=None):
-        if date is None:
+    def get_exchange_rate(self, first_currency: str, second_currency: str, date=None):
+        """
+        Get latest exchange date or on specified date.
+
+        Args:
+            first_currency: 'From' currency
+            second_currency: 'To' currency
+            date: Date of exchange rate, by default latest
+
+        Returns:
+            exchange rate for specified date
+        """
+        if not date:
             url = f"{self.source_address}latest?base={first_currency}&symbols={second_currency}"
         else:
             url = f"{self.source_address}{date}?base={first_currency}&symbols={second_currency}"
 
-        response = requests.request(
-            method="GET",
+        response = requests.get(
             url=url,
             headers=self.headers,
         )
 
-        if response.status_code == 200:
+        if response.status_code == requests.codes.ok:
             return response.json().get("rates", {})[second_currency]
         else:
             raise ValueError(
-                f"Wrong status code. Expected: 200, Actual {response.status_code}"
+                f"Wrong status code. Expected: {requests.codes.ok}, Actual {response.status_code}"
             )
--- currency_converter/tests/test_currency_converter.py
+++ currency_converter/tests/test_currency_converter.py
@@ -1,12 +1,20 @@
+"""Currency Converter tests."""
 import pytest
+import requests
 import responses
 
 from currency_converter.currency_converter import CurrencyRatesAPIConnection
 
 
+@pytest.fixture
+def api_connection() -> CurrencyRatesAPIConnection:
+    """Fixture returning connection to CurrencyRatesAPI."""
+    return CurrencyRatesAPIConnection()
+
+
 @responses.activate
-def test_get_currency_names_and_symbols():
-    c = CurrencyRatesAPIConnection()
+def test_get_currency_names_and_symbols(api_connection):
+    """Check if currency names and their symbols are read correctly from API."""
     responses.add(
         method=responses.GET,
         url="https://api.apilayer.com/exchangerates_data/symbols",
@@ -17,30 +25,32 @@ def test_get_currency_names_and_symbols():
         status=200,
     )
 
-    assert c.get_currency_names_and_symbols() == {
+    assert api_connection.get_currency_names_and_symbols() == {
         "AED": "United Arab Emirates Dirham",
         "AFN": "Afghan Afghani",
     }
 
 
 @responses.activate
-def test_get_currency_names_and_symbols_status_404_should_raise_error():
-    c = CurrencyRatesAPIConnection()
+def test_get_currency_names_and_symbols_status_404_should_raise_error(api_connection):
+    """Check if 404 error is raised when API returns 404 error for list of currences."""
     responses.add(
         method=responses.GET,
         url="https://api.apilayer.com/exchangerates_data/symbols",
         json={"success": False},
-        status=404,
+        status=requests.codes.not_found,
     )
     with pytest.raises(
-        ValueError, match="Wrong status code. Expected: 200, Actual 404"
+        ValueError,
+        match=f"Wrong status code. Expected: {requests.codes.ok}, Actual {requests.codes.not_found}",
     ):
-        c.get_currency_names_and_symbols()
+        api_connection.get_currency_names_and_symbols()
 
 
 @responses.activate
-def test_get_exchange_rate_latest_rate():
-    c = CurrencyRatesAPIConnection()
+def test_get_exchange_rate_latest_rate(api_connection):
+    """Check if correct exchange rate is read for latest date."""
+    expected_rate = 4.676721
     responses.add(
         method=responses.GET,
         url="https://api.apilayer.com/exchangerates_data/latest?base=EUR&symbols=PLN",
@@ -49,64 +59,71 @@ def test_get_exchange_rate_latest_rate():
             "timestamp": 1682763304,
             "base": "EUR",
             "date": "2023-04-29",
-            "rates": {"PLN": 4.631698},
+            "rates": {"PLN": expected_rate},
         },
-        status=200,
-    )
-
-    assert c.get_exchange_rate(first_currency="EUR", second_currency="PLN") == 4.631698
-
-
-@responses.activate
-def test_get_exchange_rate_latest_rate_status_404_should_raise_error():
-    c = CurrencyRatesAPIConnection()
-    responses.add(
-        method=responses.GET,
-        url="https://api.apilayer.com/exchangerates_data/latest?base=EUR&symbols=PLN",
-        json={
-            "success": False,
-            "timestamp": 1682763304,
-            "base": "EUR",
-            "date": "2023-04-29",
-            "rates": {},
-        },
-        status=404,
-    )
-
-    with pytest.raises(
-        ValueError, match="Wrong status code. Expected: 200, Actual 404"
-    ):
-        c.get_exchange_rate(first_currency="EUR", second_currency="PLN")
-
-
-@responses.activate
-def test_get_exchange_rate_historical_rate():
-    c = CurrencyRatesAPIConnection()
-    responses.add(
-        method=responses.GET,
-        url="https://api.apilayer.com/exchangerates_data/2023-04-03?base=EUR&symbols=PLN",
-        json={
-            "success": True,
-            "timestamp": 1680566399,
-            "historical": True,
-            "base": "EUR",
-            "date": "2023-04-03",
-            "rates": {"PLN": 4.676721},
-        },
-        status=200,
+        status=requests.codes.ok,
     )
 
     assert (
-        c.get_exchange_rate(
-            first_currency="EUR", second_currency="PLN", date="2023-04-03"
-        )
-        == 4.676721
+        api_connection.get_exchange_rate(first_currency="EUR", second_currency="PLN")
+        == expected_rate
     )
 
 
 @responses.activate
-def test_get_exchange_rate_historical_rate_status_404_should_raise_error():
-    c = CurrencyRatesAPIConnection()
+def test_get_exchange_rate_latest_rate_status_404_should_raise_error(api_connection):
+    """Check if 404 error is raised when API returns 404 error for getting latest rate."""
+    responses.add(
+        method=responses.GET,
+        url="https://api.apilayer.com/exchangerates_data/latest?base=EUR&symbols=PLN",
+        json={
+            "success": False,
+            "timestamp": 1682763304,
+            "base": "EUR",
+            "date": "2023-04-29",
+            "rates": {},
+        },
+        status=requests.codes.not_found,
+    )
+
+    with pytest.raises(
+        ValueError,
+        match=f"Wrong status code. Expected: {requests.codes.ok}, Actual {requests.codes.not_found}",
+    ):
+        api_connection.get_exchange_rate(first_currency="EUR", second_currency="PLN")
+
+
+@responses.activate
+def test_get_exchange_rate_historical_rate(api_connection):
+    """Check if correct exchange rate is read for historical date."""
+    expected_rate = 4.676721
+    responses.add(
+        method=responses.GET,
+        url="https://api.apilayer.com/exchangerates_data/2023-04-03?base=EUR&symbols=PLN",
+        json={
+            "success": True,
+            "timestamp": 1680566399,
+            "historical": True,
+            "base": "EUR",
+            "date": "2023-04-03",
+            "rates": {"PLN": expected_rate},
+        },
+        status=requests.codes.ok,
+    )
+
+    assert (
+        api_connection.get_exchange_rate(
+            first_currency="EUR", second_currency="PLN", date="2023-04-03"
+        )
+        == expected_rate
+    )
+
+
+@responses.activate
+def test_get_exchange_rate_historical_rate_status_404_should_raise_error(
+    api_connection,
+):
+    """Check if 404 error is raised when API returns 404 error for getting historical rate."""
     responses.add(
         method=responses.GET,
         url="https://api.apilayer.com/exchangerates_data/2023-04-03?base=EUR&symbols=PLN",
@@ -124,6 +141,6 @@ def test_get_exchange_rate_historical_rate_status_404_should_raise_error():
     with pytest.raises(
         ValueError, match="Wrong status code. Expected: 200, Actual 404"
     ):
-        c.get_exchange_rate(
+        api_connection.get_exchange_rate(
             first_currency="EUR", second_currency="PLN", date="2023-04-03"
         )
